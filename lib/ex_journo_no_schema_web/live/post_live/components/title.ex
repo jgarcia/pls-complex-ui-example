a980261e82ec368831539c98d0c31ae3298c6868
Joel G Martinez <joel@jgarciam.net>
2025-02-28 22:54:12 -0600

    Light controls maybe

--- lib/ex_journo_no_schema_web/live/post_live/components/title.ex
+++ lib/ex_journo_no_schema_web/live/post_live/components/title.ex
@@ -12,7 +12,7 @@ defmodule ExJournoNoSchemaWeb.PostLive.Components.Title do
           name="title"
           value={@title}
           type="text"
-          class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 -dark:bg-gray-700 -dark:border-gray-600 -dark:placeholder-gray-400 -dark:text-white -dark:focus:ring-blue-500 -dark:focus:border-blue-500"
         />
       </div>
     </form>
